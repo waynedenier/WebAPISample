57a23720c15c2f33debfd22b78ea4435a1bb5cb1
Wayne Denier II <wayne.denier@gmail.com>
2018-04-23 09:09:48 -0400

    Created FileUpload controller.

---
+++ WebAPISample/Controllers/FileUploadController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace WebAPISample.Controllers
+{
+    public class FileUploadController : ApiController
+    {
+        // POST api/fileupload
+        public HttpResponseMessage Post()
+        {
+            var httpRequest = HttpContext.Current.Request;
+            if (httpRequest.Files.Count < 1)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            foreach (string file in httpRequest.Files)
+            {
+                var postedFile = httpRequest.Files[file];
+                var filePath = HttpContext.Current.Server.MapPath("~/" + postedFile.FileName);
+                postedFile.SaveAs(filePath);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.Created);
+        }
+    }
+}
